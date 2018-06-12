1991f62092eeeafe2cc81edb52f0ecf1d8d4434f
Rafael França <euconectei@FrancaMac.local>
2018-06-11 21:07:49 -0300

    tentativa(6) de build do react

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  API_URL: 'http://localhost:3005',
+  API_URL: 'http://localhost:3000',
+  // API_URL: 'http://localhost:3005',
   SITE_TITLE: 'Chat Circles',
-  SITE_URL: 'http://localhost:3000',
+  SITE_URL: 'http://chat-circles.euconectei.com.br',
+  // SITE_URL: 'http://localhost:3000',
 }
